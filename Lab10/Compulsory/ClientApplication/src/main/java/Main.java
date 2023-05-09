87732dc6082b8ef028f368cc1c8cf065c7d26474
Codreanu Robert <codreanu.robert@outlook.com>
2023-05-09 16:56:56 +0300

    useless java files deleted

--- Lab10/Compulsory/ClientApplication/src/main/java/Main.java
+++
@@ -1,9 +0,0 @@
-//package org.example;
-//
-//public class Main {
-//    public static void main(String[] args) {
-//        String serverAddress = "localhost"; // change this to the server's IP address if running remotely
-//        int port = 1234; // change this to the server's port number
-//        new GameClient(serverAddress, port);
-//    }
-//}
